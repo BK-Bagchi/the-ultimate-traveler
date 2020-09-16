8891dbeeb93f0a8503907d967c43bb5626d47002
my name <you@exampl.com>
2020-09-16 14:02:19 +0600

    google facebook twitter auth added

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Main.css'
 import Body from '../Body/Body';
 import Header from '../Header/Header';
 import SignIn from '../Sign/Signin';
 import Signup from '../Sign/Signup';
-import './Main.css'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Location from '../Location/Location';
+import Firebase from '../Sign/Firebase';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Main = () => {
     return (
@@ -18,12 +19,14 @@ const Main = () => {
                     </Route>
                     <Route path="/login">
                         <SignIn />
+                        <Firebase />
                     </Route>
                     <Route path="/signup">
                         <Signup />
+                        <Firebase />
                     </Route>
                 </Switch>
-                <Location />
+                {/* <Location /> */}
             </Router>
         </main>
     );
---
+++ src/Components/Sign/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzo74QZKTjcB-CnILZjS5fYjl17_B6Yrw",
+    authDomain: "the-ultimate-traveler.firebaseapp.com",
+    databaseURL: "https://the-ultimate-traveler.firebaseio.com",
+    projectId: "the-ultimate-traveler",
+    storageBucket: "the-ultimate-traveler.appspot.com",
+    messagingSenderId: "218613434651",
+    appId: "1:218613434651:web:fa01a22041b7ae2d2fabbe"
+};
+
+export default firebaseConfig;
